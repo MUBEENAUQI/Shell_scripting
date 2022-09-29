c92b4afe155c5d5d216e89335aaeb5676c9db46c
mubeenaTechknocks <67516058+mubeenaTechknocks@users.noreply.github.com>
2022-09-29 10:15:49 +0530

    2 files

---
+++ hello.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "hello darlings"
+
+echo our shell name is $BASH
+echo our bash version is $BASH_VERSION
+echo our home directory is $HOME
+echo our present wokring directory is $PWD
+
+echo now by using variables
+name=mubeena
+echo my name is $name
+
---
+++ readdata.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo enter you name
+read name
+echo your name is $name
+
+echo enter any two colors
+read color1 color2
+
+echo first color is $color1 and second color is $color2
